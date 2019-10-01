20e201fa38c96a5b0f3327403d245fa459c42793
DESKTOP-DRVC8H6\murta <murtadha.aljama@gmail.com>
2019-10-01 03:10:22 -0700

    successfully finished the app

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -20,9 +20,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
@@ -99,12 +101,27 @@ class GameFragment : Fragment() {
                         binding.invalidateAll()
                     } else {
                         // We've won!  Navigate to the gameWonFragment.
+                        /*view.findNavController()
+                                .navigate(R.id.action_gameFragment_to_gameWonFragment)*/
+                        // Using directions to navigate to the GameWonFragment
+                        view.findNavController()
+                                .navigate(GameFragmentDirections.
+                                        actionGameFragmentToGameWonFragment(numQuestions, questionIndex))
                     }
                 } else {
-                    // Game over! A wrong answer sends us to the gameOverFragment.
+                    /*// Game over! A wrong answer sends us to the gameOverFragment.
+                    view.findNavController().
+                            navigate(R.id.action_gameFragment_to_gameOverFragment23)*/
+                    // Using directions to navigate to the GameOverFragment
+                    view.findNavController()
+                            .navigate(GameFragmentDirections.actionGameFragmentToGameOverFragment23())
                 }
             }
+
         }
+        /*val args = GameWonFragmentArgs.fromBundle(arguments!!)
+        Toast.makeText(context, "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}", Toast.LENGTH_LONG).show()
+*/
         return binding.root
     }
 
